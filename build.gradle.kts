f155964b053bbd5610bba6e72bb59081c1b87d1d
NotAlexNoyle <alexnoyle@icloud.com>
2024-08-26 10:34:48 -0100

    Update gradle to 8.8

---
+++ build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    java // Tell gradle this is a java project.
+    id("io.github.goooler.shadow") version "8.1.8"
+    eclipse // Import eclipse plugin for IDE integration.
+    kotlin("jvm") version "1.9.23" // Import kotlin jvm plugin for kotlin/java integration.
+}
+
+java {
+    // Declare java version.
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+group = "com.sorenstudios.wgamemode.WGamemode" // Declare bundle identifier.
+version = "3.1" // Declare plugin version (will be in .jar).
+val apiVersion = "1.19" // Declare minecraft server target version.
+
+tasks.named<ProcessResources>("processResources") {
+    val props = mapOf(
+        "version" to version,
+        "apiVersion" to apiVersion
+    )
+
+    inputs.properties(props) // Indicates to rerun if version changes.
+
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+repositories {
+    mavenCentral()
+
+    maven {
+        url = uri("https://repo.purpurmc.org/snapshots")  // Import Purpur API from PurpurMC's Maven repository.
+    }
+    
+    maven {
+
+        url = uri("https://maven.enginehub.org/repo/")  // Import EngineHub repository for WorldGuard API.
+    }
+}
+
+dependencies {
+    compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT") // Declare purpur API version to be packaged.
+    compileOnly("io.github.miniplaceholders:miniplaceholders-api:2.2.3") // Import MiniPlaceholders API.
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.7") // Import WorldGuard API.
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
+
+tasks.shadowJar {
+    exclude("io.github.miniplaceholders.*") // Exclude the MiniPlaceholders package from being shadowed.
+    minimize()
+}
+
+tasks.jar {
+    dependsOn(tasks.shadowJar)
+    archiveClassifier.set("part")
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("") // Use empty string instead of null
+    from("LICENSE") {
+        into("/")
+    }
+}
+
+tasks.jar {
+    dependsOn("shadowJar")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.compilerArgs.add("-parameters")
+    options.compilerArgs.add("-Xlint:deprecation") // Triggers deprecation warning messages.
+    options.encoding = "UTF-8"
+    options.isFork = true
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+        vendor = JvmVendorSpec.GRAAL_VM
+    }
+}
